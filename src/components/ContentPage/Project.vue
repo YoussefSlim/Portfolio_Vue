c8a32b86750be3ad45406ba8fd597a7f05b60a19
YoussefSlim <slimyoussef9511@gmail.com>
2021-03-08 10:16:07 +0100

    [bugFix]Responsive800 oks

--- src/components/ContentPage/Project.vue
+++ src/components/ContentPage/Project.vue
@@ -3,18 +3,9 @@
 
 <template>
   <section id="projects" class="projects">
+    <h3>Mes projets</h3>
     <div class="projects__mayfarmer">
       <img src="../../assets/myfarmer.png" alt="chez mon fermier" />
-      <div class="projects__mayfarmer-techno">
-        <ul>
-          <li>React</li>
-          <li>HTML</li>
-          <li>CSS</li>
-        </ul>
-      </div>
-    </div>
-    <div class="projects__eyamignardise">
-      <img src="" alt="" />
       <div class="projects__mayfarmer-techno">
         <ul>
           <li>React</li>
